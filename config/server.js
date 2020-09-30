5ba94fa795edc6b7af7fdce081b7c2284e52b342
David Mauskop <david@render.com>
2020-09-29 21:22:05 -0700

    Initial commit
    Generated with `yarn create strapi-app strapi-postgres-cloudinary --quickstart`

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea9a418022c8044305a8ab471025ee7b'),
+    },
+  },
+});
