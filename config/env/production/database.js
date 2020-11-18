76a22047ebb4fb0dd9ef0d13e054ea67c060129b
David Mauskop <david@render.com>
2020-11-17 23:31:53 -0800

    Make render.yaml more concise

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,22 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USER"),
-        password: env("DATABASE_PASSWORD"),
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+        },
       },
     },
-  },
-});
+  };
+};
