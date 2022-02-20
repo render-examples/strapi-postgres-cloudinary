f8899e5240c2a4f9724d5eff130775fff726cc31
Shidil Eringa <shidil@live.com>
2022-02-20 18:40:29 +0530

    Bump strapi to 4.1.0

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
     },
   },
 });
