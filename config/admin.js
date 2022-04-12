c2943a18c48730d60fc02ba3d948beff3fda2314
sangwon <sangwon@pixo.co>
2022-04-12 19:41:09 +0900

    add local env

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
   },
 });
--- config/server.js
+++ config/server.js
@@ -2,6 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS',['myKeyA', 'myKeyB']),
+  },
+  admin: {
+    url: '/dashboard',
+    secret: env('ADMIN_JWT_SECRET')
   },
 });
