ce368c190bff26f628781a9683cbbec92405a10f
hjkim <hjkim@pixo.co>
2022-05-18 16:09:06 +0900

    Frame Rest API Custom

--- src/api/frame/controllers/frame.js
+++ src/api/frame/controllers/frame.js
@@ -6,4 +6,38 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::frame.frame');
+//module.exports = createCoreController('api::frame.frame');
+module.exports = createCoreController('api::frame.frame', ({ strapi }) => ({
+
+    async find(ctx) {
+        // some custom logic here
+        ctx.query = { ...ctx.query, local: 'en' }
+        
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+
+        var returnData = [];
+        var size = data.length
+        var index;
+        for (index = 0; index < size; index++){
+            var currentData = data[index].attributes
+            returnData[index] = {
+                id : currentData.cid,
+                active : currentData.active,
+                unlockType : currentData.unlockType,
+                order : currentData.order,
+                userType : currentData.userType,
+                tag : currentData.tag,
+                svg : {
+                    type : currentData.svg.type,
+                    refName : currentData.svg.refName.data.attributes.url
+                },
+                img : {
+                    type : currentData.img.type,
+                    refName : currentData.img.refName.data.attributes.url
+                }
+            }
+        }
+        return returnData;
+    }
+}));
