21b13b6b86c28170d715972588b602c9837ebbeb
hjkim <hjkim@pixo.co>
2022-05-24 11:47:39 +0900

    collection response 정리

--- src/api/collection/controllers/collection.js
+++ src/api/collection/controllers/collection.js
@@ -6,4 +6,36 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::collection.collection');
+module.exports = createCoreController('api::collection.collection', ({ strapi }) => ({
+
+    async find(ctx) {
+        // some custom logic here
+        ctx.query = { ...ctx.query, local: 'en', populate : 'deep', pagination : {limit : -1} }
+        
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+
+        var returnData = [];
+        var size = data.length
+        var index;
+        for (index = 0; index < size; index++){
+            var currentId = data[index].id
+            var currentData = data[index].attributes
+            var childItems = currentData.items
+            var childItemSize = childItems.length
+            var childIndex;
+            var resultChildItems = [];
+            for(childIndex = 0; childIndex < childItemSize; childIndex++){
+                var childItem = childItems[childIndex]
+                resultChildItems[childIndex] = childItem.cid
+            }
+            returnData[index] = {
+                id : currentId,
+                title : currentData.title,
+                items : resultChildItems,
+                tag : currentData.tag
+            }
+        }
+        return returnData;
+    }
+}));
