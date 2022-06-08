224c6c99a5c2e31b74cfd107181e65dd33bc83aa
KanghoonYi <rkdgnsdls@gmail.com>
2022-06-08 17:07:21 +0900

    framework 수준의 error handler사용하도록 설정

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // emitErrors: true,
   app: {
     keys: env.array('APP_KEYS',['myKeyA', 'myKeyB']),
   },
