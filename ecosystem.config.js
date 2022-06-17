c29799c2706f737562c3a71d402783307f2f79a9
kanghoon-pixo <pour@pixo.co>
2022-06-17 15:04:53 +0900

    pm2 log 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
       args: 'start',
       exec_mode: 'cluster',
       instances: '2',
+      out_file: '/dev/null',
+      error_file: '/dev/null',
       source_map_support: true,
       instance_var: 'NODE_APP_INSTANCE',
     },
