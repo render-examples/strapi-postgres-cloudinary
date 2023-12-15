7a8e79ac68741112ccfd0a1588f21fbb55ba0a88
Asus-32gb <gilad@pop-os.localdomain>
2023-12-15 16:42:04 +0200

    markdown test 4

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -4,12 +4,24 @@ export interface ComponnentsPostContent extends Schema.Component {
   collectionName: 'components_componnents_post_contents';
   info: {
     displayName: 'Post_Content';
+    description: '';
   };
   attributes: {
     Section_title: Attribute.String;
-    First_paragraph: Attribute.String;
+    First_paragraph: Attribute.Text;
     section_image: Attribute.Media;
     section_link_text: Attribute.RichText;
+  };
+}
+
+export interface ComponnentsZone extends Schema.Component {
+  collectionName: 'components_componnents_zones';
+  info: {
+    displayName: 'Zone';
+  };
+  attributes: {
+    text: Attribute.String;
+    md: Attribute.RichText;
   };
 }
 
@@ -17,6 +29,7 @@ declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'componnents.post-content': ComponnentsPostContent;
+      'componnents.zone': ComponnentsZone;
     }
   }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -427,6 +427,8 @@ export interface ApiPostPost extends Schema.CollectionType {
     >;
     tags: Attribute.Relation<'api::post.post', 'oneToMany', 'api::tag.tag'>;
     More_Content_on_Post: Attribute.Component<'componnents.post-content', true>;
+    Dynamic: Attribute.DynamicZone<['componnents.zone']>;
+    markdown: Attribute.RichText;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
