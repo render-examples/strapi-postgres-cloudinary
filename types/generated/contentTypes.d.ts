b991a55317f0b0d87f41a8d373ffc02dd8852f7e
boazglick <info@spidernet.co.il>
2024-03-12 21:40:13 +0200

    Thats it

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1372,6 +1372,13 @@ export interface ApiTabTab extends Schema.CollectionType {
           localized: true;
         };
       }>;
+    Tab_type_controler: Attribute.String &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    brand: Attribute.Relation<'api::tab.tab', 'oneToOne', 'api::brand.brand'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
